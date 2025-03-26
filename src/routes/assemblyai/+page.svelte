e4fd2726b8cee2330a37c87bab506f768f9ae4cf
q <phamousq@gmail.com>
2025-03-26 11:05:47 -0500

    assemblyai and other features

--- src/routes/assemblyai/+page.svelte
+++ src/routes/assemblyai/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
     import { AssemblyAI } from 'assemblyai';
     import { GoogleGenerativeAI } from '@google/generative-ai';
-  
-    let transcriptionText: string = '';
-    let speakerUtterances: string[] = [];
-    let loading: boolean = false;
-    let error: string | null = null;
+
+    let transcriptionText: string = $state('');
+    let transcriptChapters: string[] = $state([]);
+    let transcriptUtterances: string[] = $state([]);
+    let assemblyaiLoading: boolean = $state(false);
+    let geminiLoading: boolean = $state(false);
+    let error: string | null = $state(null);
     let audioUrl: string = '';
     let audioFile: File | null = null;
-    let audioSrc: string | null = null;
-    let geminiResponse: string = '';
+    let audioSrc: string | null = $state(null);
+    let geminiResponse: string = $state('');
+    let bookmarks: number[] = $state([]);
+    let promptText: string = $state('Summarize the following transcript:');
+    let currentTime: number | null = $state(null); // seconds
+    let duration: number | null = $state(null);
+    let paused: boolean = $state(true);
+    let jumpTime: number | null = $state(null);
+
 
     function handleFileChange(event: Event) {
       const input = event.target as HTMLInputElement;
@@ -20,10 +29,10 @@
     }
   
     async function transcribeAudio() {
-      loading = true;
+      assemblyaiLoading = true;
       error = null;
       transcriptionText = '';
-      speakerUtterances = [];
+      transcriptUtterances = [];
   
       try {
         if (!import.meta.env.VITE_ASSEMBLYAI_API_KEY) {
@@ -47,14 +56,20 @@
         const data = {
           audio: audioSource,
           speaker_labels: true,
+          auto_chapters: true
         };
   
         const transcript = await client.transcripts.transcribe(data);
         transcriptionText = transcript.text || '';
   
         if (transcript.utterances) {
-          speakerUtterances = transcript.utterances.map(
+          transcriptUtterances = transcript.utterances.map(
             (utterance) => `Speaker ${utterance.speaker}: ${utterance.text}`
+          );
+        }
+        if (transcript.chapters) {
+          transcriptChapters = transcript.chapters.map(
+            (chapter) => `Chapter ${(chapter.start/1000).toFixed(2)}-${(chapter.end/1000).toFixed(2)}: ${chapter.headline}`
           );
         }
       } catch (e) {
@@ -64,41 +79,44 @@
           error = 'An unknown error occurred.';
         }
       } finally {
-        loading = false;
+        assemblyaiLoading = false;
       }
     }
     
     async function generateGeminiResponse() {
+        geminiLoading = true;
         const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-        const prompt = `Summerize the following transcript: ${transcriptionText}`;
-
+        const prompt = `${promptText}: ${transcriptionText}`;
         const result = await model.generateContent(prompt);
         geminiResponse = result.response.text();
+        geminiLoading = false;
     }
 </script>
   
 <main>
-    <div>
+    <div id="audio">
         <h1 class="text-2xl underline">Upload</h1>
         <label for="audioFile">Upload Audio File:</label>
         <input type="file" id="audioFile" accept="audio/*" onchange={handleFileChange} />
 
         {#if audioSrc}
-            <audio src={audioSrc} controls></audio>
+            <audio 
+                id="audioPlayer" 
+                src={audioSrc} 
+                controls
+                bind:currentTime
+                bind:duration
+                bind:paused
+            ></audio>
         {/if}
     </div>
-
-    <div>
+    <div id="transcription">
         <!-- <label for="audioUrl">Or Enter Audio URL:</label>
         <input type="text" id="audioUrl" bind:value={audioUrl} /> -->
-
-        
-
         <h1 class="text-2xl underline">Transcription</h1>
-        <button onclick={transcribeAudio} disabled={loading}>
-        {loading ? 'Transcribing...' : 'Transcribe Button'}
+        <button onclick={transcribeAudio} disabled={assemblyaiLoading}>
+            {assemblyaiLoading ? 'Transcribing...' : 'Transcribe Button'}
         </button>
     
         {#if error}
@@ -109,25 +127,72 @@
             <p>{transcriptionText}</p>
         {/if} -->
     
-        {#if speakerUtterances.length}
+        {#if transcriptUtterances.length}
             <ul>
-                {#each speakerUtterances as utterance}
-                <li>{utterance}</li>
+                {#each transcriptUtterances as utterance}
+                    <li>{utterance}</li>
                 {/each}
             </ul>
         {:else}
-            <p>No transcript found.</p>
+            <p>No transcript found yet.</p>
         {/if}
     </div>
-    <dir>
+    <div id="gemini">
         <h1 class="text-2xl underline">Gemini</h1>
-        <button onclick={generateGeminiResponse}>Generate Gemini Response</button>
-        {#if !geminiResponse.length}
-            <p>No response generated yet.</p>
-        {:else}
+        <input class="w-2xl h-10" type="text" bind:value={promptText} />
+        <button onclick={generateGeminiResponse} disabled={geminiLoading} >
+            {geminiLoading ? 'Generating...' : 'Generate Gemini Response'}
+        </button>
+        {#if geminiResponse.length}
             <p>{geminiResponse}</p>
+        {:else}
+            <p>No response generated yet.</p>
         {/if}
-    </dir>
+    </div>
+    <div id="chapters">
+        <h1 class="text-2xl underline">Chapters</h1>
+        {#if transcriptChapters.length}
+            <ul>
+                {#each transcriptChapters as chapter}
+                <li>{chapter}</li>
+                {/each}
+            </ul>
+        {:else}
+            <p>No chapters found.</p>
+        {/if}
+    </div>
+    <div id="bookmarks">
+        <h1 class="text-2xl underline">Bookmarks</h1>
+        <button 
+            onclick={() => bookmarks = [...bookmarks, +currentTime?.toFixed(2)]} 
+            disabled={paused}
+        >
+            Bookmark
+        </button>
+        {#if bookmarks.length}
+            <ol>
+                {#each bookmarks as bookmark}
+                    <li>
+                        <button aria-label="Go to bookmark" onclick={() => currentTime = bookmark}>{bookmark}</button>
+                    </li>
+                {/each}
+            </ol>
+        {/if}
+        <div id="jumpto">
+            <p>Jump to:</p>
+            <input type="number" bind:value={jumpTime}/>
+            <button onclick={() => currentTime = Number(jumpTime)}>Go</button>
+        </div>
+        <!-- ! add buttons for question vs important notes to the bookmarks -->
+    </div>
+    <div id="Test Output">
+        <h1 class="text-2xl underline">Test Output</h1>
+        {transcriptionText}
+        <p></p>
+        {transcriptUtterances}
+        <p></p>
+        {transcriptChapters}
+    </div>
 </main>
 
 <style>
